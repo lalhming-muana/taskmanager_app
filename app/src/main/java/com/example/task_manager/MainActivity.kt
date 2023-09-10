43e15ca48f5555a8a38373d5145908c7229e9793
lhmuana <edd_vai@yahoo.co.in>
2023-09-10 19:36:47 +0530

    initial update started

--- app/src/main/java/com/example/task_manager/MainActivity.kt
+++ app/src/main/java/com/example/task_manager/MainActivity.kt
@@ -9,7 +9,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import com.example.task_manager.ui.theme.Task_managerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +31,51 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SetUp()
                 }
             }
         }
     }
 }
+@Composable
+fun SetUp(){
+    TaskManager(
+        imagePainter = painterResource(R.drawable.ic_task_completed),
+        name = stringResource(R.string.name),
+        secondText = stringResource(R.string.second_text),
+
+    )
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun TaskManager(name: String, imagePainter: Painter, secondText: String,  modifier: Modifier = Modifier) {
+
+  Column(verticalArrangement = Arrangement.Center ,modifier = modifier){
+
+    Image(
+         painter = imagePainter,
+         contentDescription = null,
+
     )
+    Text(
+        text =name,
+        fontSize = 16.sp,
+        textAlign = TextAlign.Justify,
+    )
+
+    Text(
+        text =secondText,
+        fontSize = 10.sp,
+        textAlign = TextAlign.Justify,
+    )
+
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Task_managerTheme {
-        Greeting("Android")
+        SetUp()
     }
 }
